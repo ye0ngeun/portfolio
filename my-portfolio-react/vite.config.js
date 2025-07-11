943c9b1fb4c055df3646cff503d1859a572bf4a5
ye0ngeun <duddms2415@naver.com>
2025-07-11 17:36:57 +0900

    chore: 절대경로(@) 추가

--- my-portfolio-react/vite.config.js
+++ my-portfolio-react/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()]
+    plugins: [react(), tailwindcss()],
+    resolve: {
+    alias: [
+      { find: '@', replacement: '/src' }
+    ]
+  }
 })
